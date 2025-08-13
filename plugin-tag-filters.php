f3442375bb6cc7e17b6689b246ebc71180d945f0
Yoren Chang <yorenchang@gmail.com>
2025-08-13 13:26:47 +0900

    chore: update version and author information in package.json and plugin file

--- plugin-tag-filters.php
+++ plugin-tag-filters.php
@@ -5,7 +5,7 @@
  * Version:           1.0.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Author:            George Stephanis
+ * Author:            JT G.
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       plugin-tag-filters
